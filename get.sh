4b305c64cceebc0c680c891ecbc46a14e46b6169
parrotgeek1 <parrotgeek1@gmail.com>
2018-12-03 15:51:08 -0800

    Set correct permissions

--- get.sh
+++ get.sh
@@ -16,10 +16,63 @@ for i in `cat ../dylibs.txt`; do
 mkdir -p AssetData/payload/replace/`dirname $i`
 ../tools/decache/decache -c dyld_shared_cache_armv7 -x /$i -o AssetData/payload/replace/$i >/dev/null
 ../tools/ldid -S AssetData/payload/replace/$i
-chmod 0755 AssetData/payload/replace/$i
 done
-echo Tarring
 cd AssetData/payload/replace
+echo Setting permissions
+# this list was extracted by: lsbom AssetData/payload.bom  | grep -f 616b.txt | cut -d/ -f4- | awk '{print "chmod " $2 " " $1}' | sed 's/ 100/ 0/'
+chmod 0775 Library/Audio/Plug-Ins/HAL/VirtualAudio.plugin/VirtualAudio
+chmod 0644 System/Library/Caches/com.apple.dyld/dyld_shared_cache_armv7
+chmod 0444 System/Library/CoreServices/SystemVersion.plist
+chmod 0644 System/Library/DataClassMigrators/FaceTimeMigrator.migrator/Info.plist
+chmod 0644 System/Library/LaunchDaemons/com.apple.mobileactivation.recert.plist
+chmod 0644 System/Library/PrivateFrameworks/ApplePushService.framework/Info.plist
+chmod 0755 System/Library/PrivateFrameworks/ApplePushService.framework/apsd
+chmod 0644 System/Library/PrivateFrameworks/FTAWD.framework/Info.plist
+chmod 0644 System/Library/PrivateFrameworks/FTClientServices.framework/Info.plist
+chmod 0644 System/Library/PrivateFrameworks/FTServices.framework/Info.plist
+chmod 0644 System/Library/PrivateFrameworks/GameKitServices.framework/Frameworks/AVConference.framework/Info.plist
+chmod 0644 System/Library/PrivateFrameworks/GameKitServices.framework/Frameworks/GKSPerformance.framework/Info.plist
+chmod 0644 System/Library/PrivateFrameworks/GameKitServices.framework/Frameworks/ICE.framework/Info.plist
+chmod 0644 System/Library/PrivateFrameworks/GameKitServices.framework/Frameworks/LegacyHandle.framework/Info.plist
+chmod 0644 System/Library/PrivateFrameworks/GameKitServices.framework/Frameworks/SimpleKeyExchange.framework/Info.plist
+chmod 0644 System/Library/PrivateFrameworks/GameKitServices.framework/Frameworks/ViceroyTrace.framework/Info.plist
+chmod 0644 System/Library/PrivateFrameworks/GameKitServices.framework/Frameworks/snatmap.framework/Info.plist
+chmod 0644 System/Library/PrivateFrameworks/GameKitServices.framework/Info.plist
+chmod 0644 System/Library/PrivateFrameworks/IMCore.framework/Info.plist
+chmod 0644 System/Library/PrivateFrameworks/IMCore.framework/imagent.app/Info.plist
+chmod 0755 System/Library/PrivateFrameworks/IMCore.framework/imagent.app/imagent
+chmod 0644 System/Library/PrivateFrameworks/IMDAppleServices.framework/Info.plist
+chmod 0644 System/Library/PrivateFrameworks/IMDMessageServices.framework/Info.plist
+chmod 0755 System/Library/PrivateFrameworks/IMDMessageServices.framework/XPCServices/IMDMessageServicesAgent.xpc/IMDMessageServicesAgent
+chmod 0644 System/Library/PrivateFrameworks/IMDMessageServices.framework/XPCServices/IMDMessageServicesAgent.xpc/Info.plist
+chmod 0644 System/Library/PrivateFrameworks/IMDPersistence.framework/Info.plist
+chmod 0644 System/Library/PrivateFrameworks/IMDaemonCore.framework/Info.plist
+chmod 0644 System/Library/PrivateFrameworks/IMFoundation.framework/Info.plist
+chmod 0755 System/Library/PrivateFrameworks/IMFoundation.framework/XPCServices/IMRemoteURLConnectionAgent.xpc/IMRemoteURLConnectionAgent
+chmod 0644 System/Library/PrivateFrameworks/IMFoundation.framework/XPCServices/IMRemoteURLConnectionAgent.xpc/Info.plist
+chmod 0644 System/Library/PrivateFrameworks/IMTranscoding.framework/Info.plist
+chmod 0755 System/Library/PrivateFrameworks/IMTranscoding.framework/XPCServices/IMTranscoderAgent.xpc/IMTranscoderAgent
+chmod 0644 System/Library/PrivateFrameworks/IMTranscoding.framework/XPCServices/IMTranscoderAgent.xpc/Info.plist
+chmod 0644 System/Library/PrivateFrameworks/IMTransferServices.framework/Info.plist
+chmod 0755 System/Library/PrivateFrameworks/IMTransferServices.framework/XPCServices/IMTransferAgent.xpc/IMTransferAgent
+chmod 0644 System/Library/PrivateFrameworks/IMTransferServices.framework/XPCServices/IMTransferAgent.xpc/Info.plist
+chmod 0644 System/Library/PrivateFrameworks/IncomingCallFilter.framework/Info.plist
+chmod 0644 System/Library/PrivateFrameworks/MMCSServices.framework/Info.plist
+chmod 0644 System/Library/PrivateFrameworks/Marco.framework/Info.plist
+chmod 0755 System/Library/PrivateFrameworks/Marco.framework/marcoagent
+chmod 0644 System/Library/PrivateFrameworks/MobileActivation.framework/Info.plist
+chmod 0644 System/Library/PrivateFrameworks/OpenCL.framework/cl_kernel.armv7.pch
+chmod 0755 usr/libexec/lockbot
+chmod 0755 usr/libexec/lockdownd
+chmod 0755 usr/libexec/mobile_recert
+chmod 0755 usr/libexec/securityd
+chmod 0755 usr/libexec/vpnagent
+chmod 0644 usr/standalone/update/ramdisk/H3SURamDisk.dmg
+chmod 0644 System/Library/Messages/PlugIns/SMS.imservice/SMS
+chmod 0644 System/Library/SpringBoardPlugins/SIMToolkitUI.servicebundle/SIMToolkitUI
+chmod 0644 System/Library/VideoProcessors/Highlight.videoprocessor
+chmod 0644 usr/lib/libTelephonyIOKitDynamic.dylib
+echo Tarring
 ../../../../tools/gtar --owner 0 --group 0 -T ../../../../616b.txt -cf ../../../../616.tar
 cd ../../../..
 echo Cleaning
