64c67f3f6864e52d476d8dc27c2960abb85be2fd
parrotgeek1 <parrotgeek1@gmail.com>
2018-12-03 14:41:02 -0800

    duplicate

--- get.sh
+++ get.sh
@@ -3,7 +3,6 @@ echo Downloading iPod 6.1.6
 rm -rf 616 61*.zip 616b.txt 616.tar
 cat 616.txt | sed 's@^AssetData/payload/replace/@@' > 616b.txt
 cat dylibs.txt >> 616b.txt
-echo usr/lib/libTelephonyIOKitDynamic.dylib >> 616b.txt
 curl -# -L -o 616.zip http://appldnld.apple.com/iOS6.1/031-3209.20140221.VbP9o/com_apple_MobileAsset_SoftwareUpdate/0f3e181913166c8e828d946a545d02cfc08c8e02.zip
 echo Extracting
 mkdir 616
