fc8e5be971c8a8bb2e1ec67d1195ec2534d21b53
Ethan Nelson-Moore <parrotgeek1@gmail.com>
2021-05-12 13:47:50 -0700

    Some zipinfos don't have -1, use the system one which does

--- scripts/04_createIPSW.sh
+++ scripts/04_createIPSW.sh
@@ -29,7 +29,7 @@ cd work/712
 bcfg=`cat ../bcfg`
 zip -d -qq ../tmp.ipsw "Firmware/all_flash/all_flash.${bcfg}.production/battery*.img3"
 zip -d -qq ../tmp.ipsw "Firmware/all_flash/all_flash.${bcfg}.production/glyph*.img3"
-(zipinfo -1 ../tmp.ipsw | grep '^Firmware/all_flash/.*img3$'; ls -1 Firmware/all_flash/all_flash.${bcfg}.production/*.img3 )| cut -d/ -f4 | sort | uniq > Firmware/all_flash/all_flash.${bcfg}.production/manifest
+(/usr/bin/zipinfo -1 ../tmp.ipsw | grep '^Firmware/all_flash/.*img3$'; ls -1 Firmware/all_flash/all_flash.${bcfg}.production/*.img3 )| cut -d/ -f4 | sort | uniq > Firmware/all_flash/all_flash.${bcfg}.production/manifest
 zip -qq ../tmp.ipsw Firmware/all_flash/all_flash.${bcfg}.production/*.img3 Firmware/all_flash/all_flash.${bcfg}.production/manifest
 rm -f Firmware/all_flash/all_flash.${bcfg}.production/manifest
 cd ..
